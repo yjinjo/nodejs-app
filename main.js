f87ca08cba415070b3757d05422bd764ea6051ea
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-07 19:29:30 +0900

    #2. 수정 링크 생성

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return `
     <!DOCTYPE html>
     <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body) {
       <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
-        <a href="/create">create</a>
+        ${control}
         ${body}
       </body>
     </html>  
@@ -49,7 +49,8 @@ const app = http.createServer(function (request, response) {
         const template = templateHTML(
           title,
           list,
-          `<h2>${title}</h2>${description}`
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`
         );
 
         response.writeHead(200);
@@ -66,7 +67,8 @@ const app = http.createServer(function (request, response) {
             const template = templateHTML(
               title,
               list,
-              `<h2>${title}</h2>${description}`
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
             );
 
             response.writeHead(200);
@@ -94,7 +96,8 @@ const app = http.createServer(function (request, response) {
             <input type="submit" />
           </p>
         </form>
-        `
+        `,
+        ''
       );
 
       response.writeHead(200);
