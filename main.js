1052b72497672f455da11868c3d9f51fac0e74ff
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-07 19:47:22 +0900

    #2. 수정할 정보 전송

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ const app = http.createServer(function (request, response) {
         title,
         list,
         `
-        <form action="http://localhost:3000/create_process" method="POST">
+        <form action="/create_process" method="POST">
           <p><input type="text" name="title" placeholder="title" /></p>
           <p>
             <textarea name="description" placeholder="description"></textarea>
@@ -115,6 +115,33 @@ const app = http.createServer(function (request, response) {
       fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
         response.writeHead(302, { Location: `/?id=${title}` });
         response.end();
+      });
+    });
+  } else if (pathname === '/update') {
+    fs.readdir('./data', function (err, filelist) {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+        const title = queryData.id;
+        const list = templateList(filelist);
+        const template = templateHTML(
+          title,
+          list,
+          `
+          <form action="/update_process" method="POST">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}" /></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit" />
+            </p>
+          </form>
+          `,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        );
+
+        response.writeHead(200);
+        response.end(template);
       });
     });
   } else {
