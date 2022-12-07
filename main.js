f2e66a6c47c660d3c743e671b040a8ad065c40bf
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-07 20:37:17 +0900

    #2. 글 삭제 - 글 삭제 기능 완성

--- main.js
+++ main.js
@@ -167,6 +167,19 @@ const app = http.createServer(function (request, response) {
         });
       });
     });
+  } else if (pathname === '/delete_process') {
+    let body = '';
+    request.on('data', function (data) {
+      body += data;
+    });
+    request.on('end', function () {
+      let post = qs.parse(body);
+      let id = post.id;
+      fs.unlink(`data/${id}`, function (err) {
+        response.writeHead(302, { Location: `/` });
+        response.end();
+      });
+    });
   } else {
     response.writeHead(404);
     response.end('Not Found');
