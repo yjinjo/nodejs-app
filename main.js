1f7b1d561966354cc00012f806504dd0acc094de
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-07 18:26:49 +0900

    #2. 글 생성 UI 작성

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body) {
       <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
+        <a href="/create">create</a>
         ${body}
       </body>
     </html>  
@@ -73,6 +74,31 @@ const app = http.createServer(function (request, response) {
         );
       });
     }
+  } else if (pathname === '/create') {
+    fs.readdir('./data', function (err, filelist) {
+      const title = 'WEB - create';
+
+      const list = templateList(filelist);
+
+      const template = templateHTML(
+        title,
+        list,
+        `
+        <form action="http://localhost:3000/process_create" method="POST">
+          <p><input type="text" name="title" placeholder="title" /></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit" />
+          </p>
+        </form>
+        `
+      );
+
+      response.writeHead(200);
+      response.end(template);
+    });
   } else {
     response.writeHead(404);
     response.end('Not Found');
