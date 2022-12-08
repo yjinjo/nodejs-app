856432fd059abd38c86c7e3a08a147a1e8cf6c10
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-08 17:45:58 +0900

    #3. Refactoring - Topic 3

--- main.js
+++ main.js
@@ -22,85 +22,11 @@ const app = http.createServer(function (request, response) {
   } else if (pathname === '/create_process') {
     topic.create_process(request, response);
   } else if (pathname === '/update') {
-    db.query('SELECT * FROM topic', function (error, topics) {
-      if (error) {
-        throw error;
-      }
-      db.query(
-        `SELECT * FROM topic WHERE id=?`,
-        [queryData.id],
-        function (error2, topic) {
-          if (error2) {
-            throw error2;
-          }
-
-          db.query(`SELECT * FROM author`, function (error2, authors) {
-            const list = template.list(topics);
-            const html = template.HTML(
-              topic[0].title,
-              list,
-              `
-              <form action="/update_process" method="POST">
-                <input type="hidden" name="id" value="${topic[0].id}">
-                <p><input type="text" name="title" placeholder="title" value="${
-                  topic[0].title
-                }" /></p>
-                <p>
-                  <textarea name="description" placeholder="description">${
-                    topic[0].description
-                  }</textarea>
-                </p>
-                <p>
-                  ${template.authorSelect(authors, topic[0].author_id)}
-                </p>
-                <p>
-                  <input type="submit" />
-                </p>
-              </form>
-              `,
-              `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-            );
-            response.writeHead(200);
-            response.end(html);
-          });
-        }
-      );
-    });
+    topic.update(request, response);
   } else if (pathname === '/update_process') {
-    let body = '';
-    request.on('data', function (data) {
-      body += data;
-    });
-    request.on('end', function () {
-      let post = qs.parse(body);
-      db.query(
-        `UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`,
-        [post.title, post.description, post.author, post.id],
-        function (error, result) {
-          response.writeHead(302, { Location: `/?id=${post.id}` });
-          response.end();
-        }
-      );
-    });
+    topic.update_process(request, response);
   } else if (pathname === '/delete_process') {
-    let body = '';
-    request.on('data', function (data) {
-      body += data;
-    });
-    request.on('end', function () {
-      let post = qs.parse(body);
-      db.query(
-        `DELETE FROM topic WHERE id = ?`,
-        [post.id],
-        function (error, result) {
-          if (error) {
-            throw error;
-          }
-          response.writeHead(302, { Location: `/` });
-          response.end();
-        }
-      );
-    });
+    topic.delete_process(request, response);
   } else {
     response.writeHead(404);
     response.end('Not Found');
