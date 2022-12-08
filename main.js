33a995a8d77d36db6147977f5e1119152ab9b289
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-08 15:59:08 +0900

    #3. 글 수정 기능 구현

--- main.js
+++ main.js
@@ -117,32 +117,39 @@ const app = http.createServer(function (request, response) {
       );
     });
   } else if (pathname === '/update') {
-    fs.readdir('./data', function (err, filelist) {
-      const filteredId = path.parse(queryData.id).base;
-      fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-        const title = queryData.id;
-        const list = template.list(filelist);
-        const html = template.HTML(
-          title,
-          list,
-          `
-          <form action="/update_process" method="POST">
-            <input type="hidden" name="id" value="${title}">
-            <p><input type="text" name="title" placeholder="title" value="${title}" /></p>
-            <p>
-              <textarea name="description" placeholder="description">${description}</textarea>
-            </p>
-            <p>
-              <input type="submit" />
-            </p>
-          </form>
-          `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-        );
-
-        response.writeHead(200);
-        response.end(html);
-      });
+    db.query('SELECT * FROM topic', function (error, topics) {
+      if (error) {
+        throw error;
+      }
+      db.query(
+        `SELECT * FROM topic WHERE id=?`,
+        [queryData.id],
+        function (error2, topic) {
+          if (error2) {
+            throw error2;
+          }
+          const list = template.list(topics);
+          const html = template.HTML(
+            topic[0].title,
+            list,
+            `
+            <form action="/update_process" method="POST">
+              <input type="hidden" name="id" value="${topic[0].id}">
+              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}" /></p>
+              <p>
+                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+              </p>
+              <p>
+                <input type="submit" />
+              </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+        }
+      );
     });
   } else if (pathname === '/update_process') {
     let body = '';
