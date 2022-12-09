c17d4d5066214f666f48edd53d884ccb68c24660
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-09 09:43:50 +0900

    #4. 페이지 생성 구현

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const template = require('./lib/template');
 const path = require('path');
 const sanitizeHtml = require('sanitize-html');
+const qs = require('querystring');
 
 app.get('/', (req, res) => {
   fs.readdir('./data', function (error, filelist) {
@@ -42,6 +43,46 @@ app.get('/page/:pageId', (req, res) => {
           </form>`
       );
       res.send(html);
+    });
+  });
+});
+
+app.get('/create', (req, res) => {
+  fs.readdir('./data', function (error, filelist) {
+    const title = 'WEB - create';
+    const list = template.list(filelist);
+    const html = template.HTML(
+      title,
+      list,
+      `
+      <form action="/create_process" method="POST">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+    `,
+      ''
+    );
+    res.send(html);
+  });
+});
+
+app.post('/create_process', (req, res) => {
+  let body = '';
+  req.on('data', function (data) {
+    body = body + data;
+  });
+  req.on('end', function () {
+    const post = qs.parse(body);
+    const title = post.title;
+    const description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+      res.writeHead(302, { Location: `/?id=${title}` });
+      res.end();
     });
   });
 });
