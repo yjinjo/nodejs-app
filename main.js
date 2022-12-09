cbeff2d6ed5bd9c806c70317a73f44a195dc2fb3
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-09 09:15:35 +0900

    #4. 홈페이지 구현

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const template = require('./lib/template');
 
 app.get('/', (req, res) => {
-  res.send('/');
+  fs.readdir('./data', function (error, filelist) {
+    const title = 'Welcome';
+    const description = 'Hello, Node.js';
+    const list = template.list(filelist);
+    const html = template.HTML(
+      title,
+      list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    );
+    res.send(html);
+  });
 });
 
 app.get('/page', (req, res) => {
