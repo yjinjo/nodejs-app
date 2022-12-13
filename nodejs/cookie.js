39c1ddb8c3aadcad919bbf744582183e28814918
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-13 09:09:44 +0900

    #6. 쿠키 읽기

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,8 +1,15 @@
 const http = require('http');
+const cookie = require('cookie');
+
 http
   .createServer((req, res) => {
+    let cookies = {};
+    if (req.headers.cookie !== undefined) {
+      cookies = cookie.parse(req.headers.cookie);
+    }
+    console.log(cookies.yummy_cookie);
     res.writeHead(200, {
-      'Set-Cookie': ['yummy-cookie=choco', 'tasty_cookie=strawberry'],
+      'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry'],
     });
     res.end('Cookie!!');
   })
