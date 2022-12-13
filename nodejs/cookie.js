63bb29adf990215b48488242e0a19b8e55329e23
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-13 09:40:02 +0900

    #5. Secure & HttpOnly

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -13,6 +13,8 @@ http
         'yummy_cookie=choco',
         'tasty_cookie=strawberry',
         `Permanent=cookies; Max-Age=${60 * 60 * 24 * 30}`,
+        'Secure=Secure; Secure',
+        'HttpOnly=HttpOnly; HttpOnly',
       ],
     });
     res.end('Cookie!!');
